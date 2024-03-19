407e5537ba069f0ddea6726d7a13404ece75417f
BigBlueYoshi <144458971+BigBlueYoshi@users.noreply.github.com>
2024-03-19 09:45:27 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+const bob = document.querySelector('.face'
+)
+let count = 0
+function moveBob(){
+    count += 50
+    console.log(count)
+    bob.getElementsByClassName.left = count + 'px'
+}
+function addEventListener('click', moveBob)
